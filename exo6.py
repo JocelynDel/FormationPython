16ca1bd0931e6cf7a2321c664b5a52c2bffdc226
Jocelyn Delansorne <jdelansorne@gmail.com>
2021-09-22 23:54:11 +0200

    fini

--- exo6.py
+++ exo6.py
@@ -12,3 +12,34 @@ La longeur sera donnée par une saisie utilisateur
 ex: longueur: 8 => Hn_y9l2%
 '''
 print("*** EXO 5: Générateur de mot de passe ***")
+from random import choice, randint
+
+alphabetMin = [ chr(i) for i in range (97,123) ]
+alphabetMaj = [ chr(i) for i in range (65,91) ]
+digit = [ chr(i) for i in range (48,58) ]
+special = [ chr(i) for i in range (33,48)]
+
+
+
+def password(n, min= True, maj = True, chiffre = True, cspecial = True):
+    alphabets = dict()
+    key = 0
+    if min:
+        alphabets[key] = alphabetMin
+        key += 1
+    if maj:
+        alphabets[key] = alphabetMaj
+        key += 1
+    if chiffre:
+        alphabets[key] = digit
+        key += 1
+    if cspecial:
+        alphabets[key] = special
+        key += 1
+    pwd = ''
+    for i in range(n):
+        s = randint(0,key-1)
+        pwd += choice( alphabets[s])
+    return pwd
+
+print(password(5))
