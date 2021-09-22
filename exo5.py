b12080cc565be2d22ea4573e59f32036547255c5
Jocelyn Delansorne <jdelansorne@gmail.com>
2021-09-22 16:30:01 +0200

    énoncé

---
+++ exo5.py
@@ -0,0 +1,13 @@
+'''
+*** Exo 5: flags => flagsBis ***
+Créer un programme qui produira un dossier flagsBis
+Ce dossier contiendra tous les fichier png d'origine mais
+renommés en ne conservant que les deux premières lettres.
+Ces lettres seront en masjuscule.
+ex: 
+  exos/flags/allemagne.png => exos/flagsBis/AL.png
+  exos/flags/belgique.png => exos/flagsBis/BE.png
+
+Le fichier missing.png devra être ignoré
+'''
+print("*** EXO 5: flags => flagsBis ***")
